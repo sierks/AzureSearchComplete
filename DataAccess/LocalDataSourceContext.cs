a0d83c0e3c925cfb1ed873d692f33e8fa85b2662
sierks <jesse.sierks@gmail.com>
2016-05-15 19:24:29 -0500

    Add files via upload

---
+++ DataAccess/LocalDataSourceContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AzureSearchComplete.DataAccess
+{
+    public class LocalDataSourceContext : System.Data.Entity.DbContext
+    {
+        public System.Data.Entity.DbSet<AzureSearchComplete.Models.SuperHeroModel> MarvelSuperHeroes { get; set; }
+    }
+}
